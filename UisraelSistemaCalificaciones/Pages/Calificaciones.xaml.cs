a815b264534c6fc15dce21c57e0b4b2f3d3de393
christian Franco <christian.franco@sistemplan.net>
2021-10-26 12:56:39 -0500

    Agregar archivos de proyecto.

---
+++ UisraelSistemaCalificaciones/Pages/Calificaciones.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UisraelSistemaCalificaciones
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calificaciones : ContentPage
+    {
+
+        /*
+         * Declarar variables
+         */
+        string TxtNotaSeguimineto1;
+        string TxtNotaExamen1;
+        string TxtNotaSeguimineto2;
+        string TxtNotaExamen2;
+
+        double NotaSeguimineto1 = 0;
+        double NotaExamen1 = 0;
+        double NotaParcial1 = 0;
+
+        double NotaSeguimineto2 = 0;
+        double NotaExamen2 = 0;
+        double NotaParcial2 = 0;
+
+        double NotaFinal = 0;
+        public Calificaciones(string user)
+        {
+            InitializeComponent();
+            txtUser.Text = $"Bienvenido(a), {user}";
+        }
+
+        private void btnCalcularNotaFinal(object sender, EventArgs e)
+        {
+            try
+            {
+
+                TxtNotaSeguimineto1 = txtNotaSeguimineto1.Text;
+                TxtNotaExamen1 = txtNotaExamen1.Text;
+                TxtNotaSeguimineto2 = txtNotaSeguimineto2.Text;
+                TxtNotaExamen2 = txtNotaExamen2.Text;
+
+
+                /*
+                 * Validar valores nulos
+                 */
+                validarNumero(TxtNotaSeguimineto1, "Nota de seguimiento 1");
+                validarNumero(TxtNotaExamen1, "Nota de exàmen 1");
+                validarNumero(TxtNotaSeguimineto2, "Nota de seguimiento 2");
+                validarNumero(TxtNotaExamen2, "Nota de exàmen 2");
+
+                /*
+                 * Realizar la operaciòn
+                 */
+
+                NotaSeguimineto1 = Convert.ToDouble(TxtNotaSeguimineto1) * 0.3;
+                NotaExamen1 = Convert.ToDouble(TxtNotaExamen1) * 0.2;
+                NotaParcial1 = NotaSeguimineto1 + NotaExamen1;
+                txtNotaParcial1.Text = $"{NotaParcial1}";
+
+                NotaSeguimineto2 = Convert.ToDouble(TxtNotaSeguimineto2) * 0.3;
+                NotaExamen2 = Convert.ToDouble(TxtNotaExamen2) * 0.2;
+                NotaParcial2 = NotaSeguimineto2 + NotaExamen2;
+                txtNotaParcial2.Text = $"{NotaParcial2}";
+
+                NotaFinal = NotaParcial1 + NotaParcial2;
+                txtNotaFinal.Text = $"{NotaFinal}";
+                mostrarMensaje();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("No se pudo realizar la operaciòn", ex.Message, "Ok");
+            }
+        }
+
+        private void validarNumero(string value, string input)
+        {
+            validarNulos(value, input);
+            validarNumeroMaximo(value, input);
+        }
+
+        private void validarNulos(string value, string input)
+        {
+            if (value == null)
+            {
+                throw new Exception($"Debe de proporcionar un valor para {input}");
+            }    
+        }
+
+        private void validarNumeroMaximo(string value, string input)
+        {
+            if (Convert.ToDouble(value) > 10)
+            {
+                throw new Exception($"No puede ingresar un nùmero mayor a 10 en  {input}");
+            }
+        }
+
+        private void mostrarMensaje()
+        {
+            if (NotaFinal >= 7)
+            {
+                DisplayAlert("Estado", "Aprobado", "Ok");
+            }else if (NotaFinal >= 5 && NotaFinal <= 6.9)
+            {
+                DisplayAlert("Estado", "Complementario", "Ok");
+            }
+            else if(NotaFinal < 5)
+            {
+                DisplayAlert("Estado", "Reprovado", "Ok");
+            }
+        }
+    }
+}
---
+++ UisraelSistemaCalificaciones/Pages/Login.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UisraelSistemaCalificaciones
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declarar variables
+                string TxtUser = txtUser.Text;
+                string TxtPassword = txtPassword.Text;
+
+                //Vlidar campos vacìos o nulos
+                validateNullOrEmpty(TxtUser);
+                validateNullOrEmpty(TxtPassword);
+
+                //Verificar contraseña
+               await validateAccess(TxtUser, TxtPassword);
+            }
+            catch (Exception ex)
+            {
+               await DisplayAlert("No se pudo realizar la operaciòn", ex.Message, "Ok");
+            }
+        }
+
+        private void validateNullOrEmpty(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new Exception($"Ingrese los valores solicitados");
+            }
+        }
+
+        private async Task validateAccess(string user, string password)
+        {
+            if (user == "estudiante2021" && password == "uisrael2021")
+            {
+                await Navigation.PushAsync(new Calificaciones(user));
+            }
+            else
+            {
+                throw new Exception($"Las credenciales son incorrectas");
+            }
+        }
+    }
+}
